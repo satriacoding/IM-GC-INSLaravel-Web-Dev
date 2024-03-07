23d35dc5a297929593b0aeb60902f03aa8de1fa0
Satria Rhama <118911569+satriacoding@users.noreply.github.com>
2024-03-07 10:27:24 +0700

    Tugas OOP PHP

---
+++ Minggu-Ke-2/OOP-PHP/ape.php
@@ -0,0 +1,11 @@
+<?php
+class ape extends animal
+{
+    public $yell;
+    public function yell()
+{
+    return $this->yell = "Auoooo";
+}
+}
+
+?>
---
+++ Minggu-Ke-2/OOP-PHP/index.php
@@ -0,0 +1,31 @@
+<?php
+ require 'animal.php';
+ require 'ape.php';
+ require 'frog.php';
+
+
+ $sheep = new animal();
+ $sheep-> setName("shaun");
+
+echo "Name: ".$sheep-> getName()."<br>";
+echo "legs: ".$sheep-> legs()."<br>";
+echo "cold blooded: ".$sheep->cold_blooded()."<br><br>";
+
+$kodok = new frog();
+$kodok-> setName("buduk");
+
+echo "Name: ".$kodok-> getName()."<br>";
+echo "legs: ".$kodok-> legs()."<br>";
+echo "cold blooded: ".$kodok->cold_blooded()."<br>";
+echo "jump: ".$kodok->jump()."<br><br>";
+
+$sungokong = new ape();
+$sungokong-> setName("kera sakti");
+
+echo "Name: ".$sungokong-> getName()."<br>";
+echo "legs: ".$sungokong-> leg()."<br>";
+echo "cold blooded: ".$sungokong-> cold_blooded()."<br>";
+echo "yell: ".$sungokong-> yell()."<br><br>";
+
+
+?>
